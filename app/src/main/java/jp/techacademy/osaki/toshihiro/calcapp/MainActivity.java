2750abd2499e886676ebc18f03a60035b9905bb8
toshihiro-osaki <lkjlkjozaki10>
2018-11-23 19:13:49 +0900

    再提出

--- app/src/main/java/jp/techacademy/osaki/toshihiro/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/osaki/toshihiro/calcapp/MainActivity.java
@@ -22,13 +22,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button1.setOnClickListener(this);
 
         Button button2 = (Button) findViewById(R.id.button2);
-        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
 
         Button button3 = (Button) findViewById(R.id.button3);
-        button1.setOnClickListener(this);
+        button3.setOnClickListener(this);
 
         Button button4 = (Button) findViewById(R.id.button4);
-        button1.setOnClickListener(this);
+        button4.setOnClickListener(this);
 
         mEditText1 = (EditText) findViewById(R.id.editText1);
         mEditText2 = (EditText) findViewById(R.id.editText2);
@@ -37,57 +37,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        double dbl1 = Double.parseDouble(mEditText1.getText().toString());
-        double dbl2 = Double.parseDouble(mEditText2.getText().toString());
-        String ans = "";
 
-        if (mEditText1.getText().toString() == "" || mEditText2.getText().toString() == "") {
-            showAlertDialog1();
-        } else {
+        //if (mEditText1.getText().toString().equals("") || mEditText2.getText().toString().equals("")) {
+        //    showAlertDialog1();
+        //} else {
             try {
-                if (v.getId() == R.id.button1) {
-                    //＋
+                double dbl1 = Double.parseDouble(mEditText1.getText().toString());
+                double dbl2 = Double.parseDouble(mEditText2.getText().toString());
+                String ans = "";
+                if (v.getId() == R.id.button1) { //＋
                     ans = mEditText1.getText().toString() + "＋" + mEditText2.getText().toString() + "＝" + String.valueOf(dbl1 + dbl2);
-                    Intent intent = new Intent(this, SecondActivity.class);
-                    intent.putExtra("ANS", ans);
-                    startActivity(intent);
 
                 } else if (v.getId() == R.id.button2) {
                     //－
                     ans = mEditText1.getText().toString() + "－" + mEditText2.getText().toString() + "＝" + String.valueOf(dbl1 - dbl2);
-                    Intent intent = new Intent(this, SecondActivity.class);
-                    intent.putExtra("ANS", ans);
-                    startActivity(intent);
 
                 } else if (v.getId() == R.id.button3) {
                     //×
                     ans = mEditText1.getText().toString() + "×" + mEditText2.getText().toString() + "＝" + String.valueOf(dbl1 * dbl2);
-                    Intent intent = new Intent(this, SecondActivity.class);
-                    intent.putExtra("ANS", ans);
-                    startActivity(intent);
 
                 } else if (v.getId() == R.id.button4) {
                     //÷
+                    if (dbl2==0){
+                        String ttl="<警告>";
+                        String msg="ゼロ割エラー";
+                        showAlertDialog(ttl,msg);
+                        return;
+                    }
                     ans = mEditText1.getText().toString() + "÷" + mEditText2.getText().toString() + "＝" + String.valueOf(dbl1 / dbl2);
-                    Intent intent = new Intent(this, SecondActivity.class);
-                    intent.putExtra("ANS", ans);
-                    startActivity(intent);
 
                 }
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("ANS", ans);
+                startActivity(intent);
             } catch (Exception e) {
                 // 例外情報 e.getMessage()
-                showAlertDialog2();
+                String ttl="<注意>";
+                String msg="数値を入力してください";
+                showAlertDialog(ttl,msg);
             }
-
-        }
-
+        //}
     }
 
-    private void showAlertDialog1() {
+    private void showAlertDialog(String ttl,String msg) {
         // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle("タイトル:注意");
-        alertDialogBuilder.setMessage("メッセージ：数字を入力してください。");
+        alertDialogBuilder.setTitle(ttl);
+        alertDialogBuilder.setMessage(msg);
 
         // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
         alertDialogBuilder.setPositiveButton("OK", null);
@@ -96,19 +92,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
-
-    private void showAlertDialog2() {
-        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle("タイトル:警告");
-        alertDialogBuilder.setMessage("メッセージ：ゼロ割エラー");
-
-        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
-        alertDialogBuilder.setPositiveButton("OK", null);
-
-        // AlertDialog2を作成して表示する
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }
-
 }
